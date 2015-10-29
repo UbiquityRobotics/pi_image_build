b12f3077f3cec4dc50e4591895fb3ba14ab3bb86
Martin Wimpress <code@flexion.org>
2015-10-29 14:12:31 +0000

    Simplfy settings.

--- build-settings.sh
+++ build-settings.sh
@@ -20,12 +20,10 @@
 ########################################################################
 
 FLAVOUR="ubuntu-mate"
-FLAVOUR_NAME="Ubuntu MATE"
 RELEASE="wily"
-VERSION="15.10.1"
+POINT=".1"
 QUALITY=""
-
-# Either 'ext4' or 'f2fs'
+# Either 'ext4' or 'f2fs' - f2fs has not been tested in while
 FS_TYPE="ext4"
 
 # Either 4, 8 or 16
@@ -36,6 +34,55 @@ FS_SIZE=4
 # - 1 make a generic rootfs tarball
 MAKE_TARBALL=0
 
+########################################################################
+#
+#      NO NEED TO EDIT BELOW HERE UNLESS YOU KNOW WHY YOU MUST         #
+#
+########################################################################
+
+# Validate the release
+if [ "${RELEASE}" != "vivid" ] && [ "${RELEASE}" != "wily" ] && [ "${RELEASE}" != "xenial" ]; then
+    echo "ERROR! ${RELEASE} is not currently supported."
+    exit 1
+else
+    if [ "${RELEASE}" == "vivid" ]; then
+        VERSION="15.04${POINT}"
+    elif [ "${RELEASE}" == "wily" ]; then
+        VERSION="15.10${POINT}"
+    elif [ "${RELEASE}" == "xenial" ]; then
+        VERSION="16.04${POINT}"
+    else
+        echo "ERROR! ${RELEASE} is not currently supported."
+        exit 1
+    fi
+fi
+
+# Validate the flavour
+if [ "${FLAVOUR}" == "ubuntu-minimal" ] || [ "${FLAVOUR}" == "ubuntu-standard" ]; then
+    FLAVOUR_NAME="Ubuntu"
+    USERNAME="ubuntu"
+    OEM_CONFIG=0
+else
+    if [ "${FLAVOUR}" == "lubuntu" ]; then
+        FLAVOUR_NAME="Lubuntu"
+    elif [ "${FLAVOUR}" == "kubuntu" ]; then
+        FLAVOUR_NAME="Kubuntu"
+    elif [ "${FLAVOUR}" == "ubuntu" ]; then
+        FLAVOUR_NAME="Ubuntu"
+    elif [ "${FLAVOUR}" == "ubuntu-gnome" ]; then
+        FLAVOUR_NAME="Ubuntu GNOME"
+    elif [ "${FLAVOUR}" == "ubuntu-mate" ]; then
+        FLAVOUR_NAME="Ubuntu MATE"
+    elif [ "${FLAVOUR}" == "xubuntu" ]; then
+        FLAVOUR_NAME="Xubuntu"
+    else
+        echo "ERROR! ${FLAVOUR} is not currently supported."
+        exit 1
+    fi
+    USERNAME="${FLAVOUR}"
+    OEM_CONFIG=1
+fi
+
 TARBALL="${FLAVOUR}-${VERSION}${QUALITY}-desktop-armhf-rootfs.tar.bz2"
 IMAGE="${FLAVOUR}-${VERSION}${QUALITY}-desktop-armhf-raspberry-pi-2.img"
 BASEDIR=${HOME}/build/${RELEASE}
@@ -45,15 +92,6 @@ DESKTOP_R=${BUILDDIR}/desktop
 DEVICE_R=${BUILDDIR}/pi2
 ARCH=$(uname -m)
 export TZ=UTC
-
-
-if [ "${FLAVOUR}" == "ubuntu-minimal" ] || [ "${FLAVOUR}" == "ubuntu-standard" ]; then
-    USERNAME="ubuntu"
-    OEM_CONFIG=0
-else
-    USERNAME="${FLAVOUR}"
-    OEM_CONFIG=1
-fi
 
 # Override OEM_CONFIG here if required. Either 0 or 1.
 # - 0 to hardcode a user.
