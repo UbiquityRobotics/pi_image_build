8727e22def61ce48dcb4ab004f9b86029a09d791
Martin Wimpress <martin.wimpress@ubuntu.com>
2017-02-04 22:03:07 +0000

    Simplify publish-image.

--- publish-image.sh
+++ publish-image.sh
@@ -21,6 +21,8 @@
 
 set -ex
 
+DEST="man"
+
 if [ -f build-settings.sh ]; then
     source build-settings.sh
 else
@@ -28,61 +30,28 @@ else
     exit 1
 fi
 
-if [ $(id -u) -ne 0 ]; then
-    echo "ERROR! Must be root."
+if [ $(id -u) -eq 0 ]; then
+    echo "ERROR! Must be a regular user."
     exit 1
 fi
 
-function make_hash() {
-    source ${HOME}/Roaming/Scripts/key
-    local FILE="${1}"
-    local HASH="${2}"
-    if [ ! -f ${FILE}.${HASH}.sign ]; then
-        if [ -f ${FILE} ]; then
-            ${HASH}sum ${FILE} > ${FILE}.${HASH}
-            sed -i -r "s/ .*\/(.+)/  \1/g" ${FILE}.${HASH}
-            gpg --default-key ${KEY} --armor --output ${FILE}.${HASH}.sign --detach-sig ${FILE}.${HASH}
-        else
-            echo "WARNING! Didn't find ${FILE} to hash."
-        fi
-    else
-        echo "Existing signature found, skipping..."
-    fi
-}
-
 function publish_image() {
-    source ${HOME}/Roaming/Scripts/dest
-    local HASH=sha256
-    if [ -n "${DEST}" ]; then
-        echo "Sending to: ${DEST}"
-        if [ ! -e "${BASEDIR}/${IMAGE}.xz" ]; then
-            xz ${BASEDIR}/${IMAGE}
-        fi
-        make_hash "${BASEDIR}/${IMAGE}.xz" ${HASH}
-        ssh ${DEST} mkdir -p ~/ISO-Mirror/${RELEASE}/armhf/
-        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz" ${DEST}:ISO-Mirror/${RELEASE}/armhf/
-        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.${HASH}" ${DEST}:ISO-Mirror/${RELEASE}/armhf/
-        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.${HASH}.sign" ${DEST}:ISO-Mirror/${RELEASE}/armhf/
-    fi
+    echo "Sending to: ${DEST}"
+    ssh matey@${DEST}.ubuntu-mate.net mkdir -p /home/matey/ISO-Mirror/${RELEASE}/armhf/
+    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
+    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.sha256" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
+    rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${IMAGE}.xz.sha256.sign" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
 }
 
 function publish_tarball() {
     if [ ${MAKE_TARBALL} -eq 1 ]; then
-        source ${HOME}/Roaming/Scripts/dest
-        local HASH=sha256
-        if [ -n "${DEST}" ]; then
-            if [ ! -e "${BASEDIR}/${TARBALL}" ]; then
-                echo "ERROR! Could not find ${TARBALL}. Exitting."
-                exit 1
-            fi
-            make_hash "${BASEDIR}/${TARBALL}" ${HASH}
-            echo "Sending to: ${DEST}"
-            rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}" ${DEST}:ISO-Mirror/${RELEASE}/armhf/
-            rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.${HASH}" ${DEST}:ISO-Mirror/${RELEASE}/armhf/
-            rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.${HASH}.sign" ${DEST}:ISO-Mirror/${RELEASE}/armhf/
-        fi
+        echo "Sending to: ${DEST}"
+        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
+        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.sha256" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
+        rsync -rvl -e 'ssh -c aes128-gcm@openssh.com' --progress "${BASEDIR}/${TARBALL}.sha256.sign" matey@${DEST}.ubuntu-mate.net:ISO-Mirror/${RELEASE}/armhf/
     fi
 }
 
 publish_image
 publish_tarball
+echo "Published to ${DEST}. Now mirror from there!"
