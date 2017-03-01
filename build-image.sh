5ec64e208798a2e542fabb053403a1908b6a8668
Rohan Agrawal <send2arohan@gmail.com>
2017-02-28 20:31:56 -0800

    source before catkin_make

--- build-image.sh
+++ build-image.sh
@@ -322,7 +322,7 @@ EOM
     
     # Make sure that permissions are still sane
     chroot $R chown -R ubuntu:ubuntu /home/ubuntu
-    chroot $R su ubuntu -c "cd /home/${USERNAME}/catkin_ws; catkin_make;"
+    chroot $R su ubuntu -c "bash -c 'cd /home/${USERNAME}/catkin_ws; source /opt/ros/kinetic/setup.bash; catkin_make;'"
 }
 
 function disable_services() {
